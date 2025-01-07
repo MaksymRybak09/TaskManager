a31857e3646c581eda0b88827cc839e77d325d31
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-07 10:40:50 +0200

    include: user module to ap module

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { validationSchema } from './../config/config-service.config'
+import { UserModule } from 'src/user/user.module'
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { validationSchema } from './../config/config-service.config'
       isGlobal: true,
       validationSchema,
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
