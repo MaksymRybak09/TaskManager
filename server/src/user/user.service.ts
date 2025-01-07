7c8fb184ffa8bfa61fb5492aecdbf7e14e985ad0
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-07 16:22:59 +0200

    add: get by id & create user functions in user service

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { AuthDTO } from 'src/auth/dto/auth.dto'
 import { PrismaService } from '../prima.service'
+import { hash } from 'argon2'
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
+  async createUser(dto: AuthDTO) {
+    const user = {
+      name: '',
+      email: dto.email,
+      password: await hash(dto.password),
+    }
+    return this.prisma.user.create({ data: user })
+  }
 
   async getByID(id: string) {
     return this.prisma.user.findUnique({
@@ -11,6 +21,12 @@ export class UserService {
       include: {
         tasks: true,
       },
+    })
+  }
+
+  async getByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
     })
   }
 }
