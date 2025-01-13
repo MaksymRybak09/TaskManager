383ed9ab370e4af5c02fa444f0642968363b919a
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-13 15:15:25 +0200

    add: register log-in form page

---
+++ client/src/pages/RegisterLogInPage/index.ts
@@ -0,0 +1,1 @@
+export { default as RegisterLogInPage } from './ui/RegisterLogInPage'
---
+++ client/src/pages/RegisterLogInPage/ui/RegisterLogInPage.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { RegisterLogInForm } from '@/features/RegisterLogInForm'
+import FormLayout from '@/shared/layout/FormLayout'
+
+function RegisterLogInPage() {
+  return (
+    <FormLayout>
+      <RegisterLogInForm />
+    </FormLayout>
+  )
+}
+
+export default RegisterLogInPage
