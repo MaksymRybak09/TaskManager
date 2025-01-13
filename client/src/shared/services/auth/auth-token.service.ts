289897fdacec51b74a6dfd9f3e59c2a20a3fe95b
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-13 10:20:10 +0200

    add: get save remove token functions

---
+++ client/src/shared/services/auth/auth-token.service.ts
@@ -0,0 +1,19 @@
+import Cookies from 'js-cookie'
+import { Tokens } from '../../constants/tokens.constants'
+
+export const getAccessToken = () => {
+  const accessToken = Cookies.get(Tokens.ACCESS_TOKEN)
+  return accessToken || null
+}
+
+export const saveTokenToStorage = (accessToken: string) => {
+  Cookies.set(Tokens.ACCESS_TOKEN, accessToken, {
+    domain: process.env.CLIENT_HOST,
+    sameSite: 'strict',
+    expires: 1,
+  })
+}
+
+export const removeTokenFromStorage = () => {
+  Cookies.remove(Tokens.ACCESS_TOKEN)
+}
