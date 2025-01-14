38effcefc27ccfc4229fb1204c3dfbb11e70524d
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-14 15:13:56 +0200

    add: statistics cell component

---
+++ client/src/shared/components/statisticsCell/StatisticsCell.tsx
@@ -0,0 +1,17 @@
+import styles from './statistics-cell.module.scss'
+
+type StatisticsCellProps = {
+  label: string
+  value: string
+}
+
+function StatisticsCell(props: StatisticsCellProps) {
+  return (
+    <div className={styles.cell}>
+      <div className={styles['cell__value']}>{props.value}</div>
+      <div className={styles['cell__title']}>{props.label}</div>
+    </div>
+  )
+}
+
+export default StatisticsCell
