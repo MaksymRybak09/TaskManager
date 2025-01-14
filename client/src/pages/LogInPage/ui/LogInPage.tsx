36cf63f23c852fc089644115cb87c04898125a2b
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-14 12:29:39 +0200

    saparate: log-in and register forms pages

---
+++ client/src/pages/LogInPage/ui/LogInPage.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { LogInForm } from '@/features/RegisterLogInForm'
+import FormLayout from '@/shared/layout/FormLayout'
+
+function RegisterPage() {
+  return (
+    <FormLayout>
+      <LogInForm />
+    </FormLayout>
+  )
+}
+
+export default RegisterPage
---
+++ client/src/pages/RegisterPage/ui/RegisterPage.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { RegisterForm } from '@/features/RegisterLogInForm'
+import FormLayout from '@/shared/layout/FormLayout'
+
+function RegisterPage() {
+  return (
+    <FormLayout>
+      <RegisterForm />
+    </FormLayout>
+  )
+}
+
+export default RegisterPage
