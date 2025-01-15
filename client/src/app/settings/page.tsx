39bf9785600e562900fa8a583f34ee615191e63c
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-15 16:38:58 +0200

    add: settings page

---
+++ client/src/app/settings/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { SettingsPage } from '@/pages/SettingsPage'
+import { NO_INDEX_PAGE } from '@/shared/constants/seo.constants'
+import { Metadata } from 'next'
+
+export const metadate: Metadata = {
+  title: 'Settings',
+  ...NO_INDEX_PAGE,
+}
+
+export default function AuthPage() {
+  return <SettingsPage />
+}
