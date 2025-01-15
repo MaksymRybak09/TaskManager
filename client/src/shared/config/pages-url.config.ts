82f042f4a3b19a0849693b5780f5a5e4a018bdf0
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-15 10:11:20 +0200

    fix: pages url

--- client/src/shared/config/pages-url.config.ts
+++ client/src/shared/config/pages-url.config.ts
@@ -1,12 +1,15 @@
 class DASHBOARD {
   private root = '/'
 
+  REGISTER = `${this.root}register`
+  LOG_IN = `${this.root}log-in`
+
   HOME = this.root
-  TASKS = `${this.root}/tasks`
-  HABITS = `${this.root}/habits`
-  TIMER = `${this.root}/timer`
-  TIME_BLOCKING = `${this.root}/time-blocking`
-  SETTINGS = `${this.root}/settings`
+  TASKS = `${this.root}tasks`
+  HABITS = `${this.root}habits`
+  TIMER = `${this.root}timer`
+  TIME_BLOCKING = `${this.root}time-blocking`
+  SETTINGS = `${this.root}settings`
 }
 
 export const DASHBOARD_PAGES = new DASHBOARD()
