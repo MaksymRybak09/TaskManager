809c998b2ebd985f47ec0f3726a37a1dc7cc6665
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-15 16:10:54 +0200

    change: use profile hook

--- client/src/shared/hooks/useProfile.ts
+++ client/src/shared/hooks/useProfile.ts
@@ -3,10 +3,10 @@ import { useQuery } from '@tanstack/react-query'
 import { userService } from '../services/user/user.service'
 
 export const useProfile = () => {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isSuccess } = useQuery({
     queryKey: ['profile'],
     queryFn: () => userService.getProfile(),
   })
 
-  return { profile: data, isLoading }
+  return { profile: data, isLoading, isSuccess }
 }
