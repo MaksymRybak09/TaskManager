12cd0485a1e98d6c0629dd505fe58694680fcc1a
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-21 12:52:36 +0200

    add: new task feature

---
+++ client/src/features/NewTaskButton/ui/NewTaskButton.tsx
@@ -0,0 +1,21 @@
+import TransparentButton from '@/shared/components/transparentButton/TransparentButton'
+import { addTask } from '@/shared/helpers/add-task'
+import { ITask } from '@/shared/types/task.types'
+import { Dispatch, SetStateAction } from 'react'
+
+type ListAddRowInputProps = {
+  filterDate?: string
+  setItems: Dispatch<SetStateAction<ITask[] | undefined>>
+}
+
+function NewTaskButton(props: ListAddRowInputProps) {
+  return (
+    <TransparentButton
+      onClick={() => addTask(props.setItems, props.filterDate)}
+    >
+      New task
+    </TransparentButton>
+  )
+}
+
+export default NewTaskButton
