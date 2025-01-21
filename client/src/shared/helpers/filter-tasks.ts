34322e4dd8a64f1831a4e84002597abff23a38db
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-21 16:12:17 +0200

    add: task filter helpers

---
+++ client/src/shared/helpers/filter-tasks.ts
@@ -0,0 +1,52 @@
+import { ITask } from '../types/task.types'
+import dayjs from 'dayjs'
+import isoWeek from 'dayjs/plugin/isoWeek'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { FILTERS } from '../constants/task-table-filters'
+import { hasValidDate } from './has-valid-date'
+
+dayjs.extend(isoWeek)
+dayjs.extend(weekOfYear)
+
+const taskFilters: any = {
+  today: (tasks: ITask[] | undefined) =>
+    tasks?.filter(
+      (item) =>
+        dayjs(item.createdAt).isSame(FILTERS.today, 'day') && !item.isCompleted,
+    ),
+  tomorrow: (tasks: ITask[] | undefined) =>
+    tasks?.filter(
+      (item) =>
+        dayjs(item.createdAt).isSame(FILTERS.tomorrow, 'day') &&
+        !item.isCompleted,
+    ),
+  ['on-this-week']: (tasks: ITask[] | undefined) =>
+    tasks?.filter(
+      (item) =>
+        !dayjs(item.createdAt).isSame(FILTERS.today, 'day') &&
+        !dayjs(item.createdAt).isSame(FILTERS.tomorrow) &&
+        dayjs(item.createdAt).isSameOrBefore(FILTERS['on-this-week']) &&
+        !item.isCompleted,
+    ),
+  ['on-next-week']: (tasks: ITask[] | undefined) =>
+    tasks?.filter(
+      (item) =>
+        dayjs(item.createdAt).isAfter(FILTERS['on-this-week']) &&
+        dayjs(item.createdAt).isBefore(FILTERS['later']) &&
+        !item.isCompleted,
+    ),
+  later: (tasks: ITask[] | undefined) =>
+    tasks?.filter(
+      (item) =>
+        (!item.createdAt ||
+          (hasValidDate(item) &&
+            dayjs(item.createdAt).isSameOrAfter(FILTERS['later']))) &&
+        !item.isCompleted,
+    ),
+  completed: (tasks: ITask[] | undefined) =>
+    tasks?.filter((item) => item.isCompleted),
+}
+
+export const filterTasks = (tasks: ITask[] | undefined, value: string) => {
+  return taskFilters[value](tasks) ?? []
+}
---
+++ client/src/shared/helpers/has-valid-date.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import { ITask } from '../types/task.types'
+
+dayjs.extend(isSameOrAfter)
+dayjs.extend(isSameOrBefore)
+
+export const hasValidDate = (item: ITask) =>
+  item.createdAt && dayjs(item.createdAt).isValid()
