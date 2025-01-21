bf2cc1947f199979520e620829f239a592e301b9
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-21 11:51:00 +0200

    add: delete task feature

---
+++ client/src/features/DeleteTaskButton/ui/DeleteTaskButton.tsx
@@ -0,0 +1,27 @@
+import TransparentButton from '@/shared/components/transparentButton/TransparentButton'
+import { useDeleteTask } from '@/shared/hooks/tasks/use-delete-task.hook'
+import { ITask } from '@/shared/types/task.types'
+import { Dispatch, SetStateAction } from 'react'
+
+type DeleteTaskButton = {
+  item: ITask
+  setItems: Dispatch<SetStateAction<ITask[] | undefined>>
+}
+
+function DeleteTaskButton(props: DeleteTaskButton) {
+  const { deleteTask, isDeletePending } = useDeleteTask()
+
+  return (
+    <TransparentButton
+      onClick={() =>
+        props.item.id
+          ? deleteTask(props.item.id)
+          : props.setItems((prev) => prev?.slice(0, -1))
+      }
+    >
+      {isDeletePending ? 'Loading...' : 'Delete task'}
+    </TransparentButton>
+  )
+}
+
+export default DeleteTaskButton
