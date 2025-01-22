a9a10fd30c23d2f5901af75ed2c2e099678a6c18
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-22 09:36:08 +0200

    add: task table feature

---
+++ client/src/features/TasksTable/ui/TasksTable.tsx
@@ -0,0 +1,65 @@
+import { NewTaskButton } from '@/features/NewTaskButton'
+import { TaskRow } from '@/features/TaskRow'
+import { FILTERS } from '@/shared/constants/task-table-filters'
+import { filterTasks } from '@/shared/helpers/filter-tasks'
+import styles from '@/shared/styles/task-table-layout.module.scss'
+import { ITask } from '@/shared/types/task.types'
+import { Draggable, Droppable } from '@hello-pangea/dnd'
+import { Dispatch, SetStateAction } from 'react'
+
+type TasksTableProps = {
+  value: string
+  label: string
+  items: ITask[] | undefined
+  setItems: Dispatch<SetStateAction<ITask[] | undefined>>
+}
+
+function TasksTable(props: TasksTableProps) {
+  return (
+    <Droppable droppableId={props.value}>
+      {(provided) => (
+        <div
+          className={styles['table']}
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          <div className={styles['table__title']}>{props.label}</div>
+          {filterTasks(props.items, props.value)?.map(
+            (item: ITask, index: number) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <TaskRow
+                      key={item.id}
+                      item={item}
+                      setItems={props.setItems}
+                    />
+                  </div>
+                )}
+              </Draggable>
+            ),
+          )}
+          {provided.placeholder}
+          <div className={styles['table__button']}>
+            {props.value !== 'completed' && (
+              <NewTaskButton
+                setItems={props.setItems}
+                filterDate={
+                  FILTERS[props.value]
+                    ? FILTERS[props.value].format()
+                    : undefined
+                }
+              />
+            )}
+          </div>
+        </div>
+      )}
+    </Droppable>
+  )
+}
+
+export default TasksTable
