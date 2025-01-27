7f09dc588c40b21f2121e850a30f14edf8c5988a
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-27 15:32:48 +0200

    add: time block entity

---
+++ client/src/entity/TimeBlock/ui/TimeBlock.tsx
@@ -0,0 +1,41 @@
+import { useTimeBlockSortable } from '@/shared/hooks/time-block/use-time-block-sortable'
+import { ITimeBlock } from '@/shared/types/time-block.types'
+import { ReactNode } from 'react'
+import timeBlockStyles from './time-block.module.scss'
+
+type TimeBlockProps = {
+  item: ITimeBlock
+  actions: ReactNode[]
+}
+
+function TimeBlock(props: TimeBlockProps) {
+  const { attributes, listeners, setNodeRef, styles } = useTimeBlockSortable(
+    props.item.id,
+  )
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={styles}
+      {...attributes}
+      {...listeners}
+      aria-describedby="time-block"
+    >
+      <div
+        className={timeBlockStyles['time-block']}
+        style={{
+          backgroundColor: props.item.color,
+          height: `${props.item.duration < 80 ? 80 : props.item.duration}px`,
+        }}
+      >
+        <div>
+          {props.item.name}
+          <p>{props.item.duration}</p>
+        </div>
+        <div>{props.actions}</div>
+      </div>
+    </div>
+  )
+}
+
+export default TimeBlock
