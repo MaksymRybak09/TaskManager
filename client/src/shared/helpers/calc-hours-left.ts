21d34c212822ad9db28438e216db62631df5fd65
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-27 15:31:53 +0200

    add: calculate hours left helper

---
+++ client/src/shared/helpers/calc-hours-left.ts
@@ -0,0 +1,7 @@
+import { ITimeBlock } from '../types/time-block.types'
+
+export const calcHoursLeft = (items: ITimeBlock[] | undefined) => {
+  const totalMinutes = items?.reduce((acc, item) => acc + item.duration, 0) || 0
+  const totalHours = Math.floor(totalMinutes / 60)
+  return 24 - totalHours
+}
