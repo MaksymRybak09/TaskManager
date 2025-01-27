544c6f9d13b5e40377f41521f94bdb80e3c096ec
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-27 15:34:01 +0200

    add: time block list

---
+++ client/src/features/TimeBlockList/index.ts
@@ -0,0 +1,1 @@
+export { default as TimeBlockList } from './ui/TimeBlockList'
---
+++ client/src/features/TimeBlockList/ui/TimeBlockList.tsx
@@ -0,0 +1,56 @@
+import { TimeBlock } from '@/entity/TimeBlock'
+import { DeleteTimeBlockButton } from '@/features/DeleteTimeBlockButton'
+import { UpdateTimeBlockButton } from '@/features/UpdateTimeBlockButton'
+import { calcHoursLeft } from '@/shared/helpers/calc-hours-left'
+import { useTimeBlockDND } from '@/shared/hooks/time-block/use-time-block-dnd'
+import { useTimeBlocks } from '@/shared/hooks/time-block/use-time-blocks'
+import { closestCenter, DndContext } from '@dnd-kit/core'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import styles from './time-block-list.module.scss'
+
+function TimeBlockList() {
+  const { items, setItems, isLoading } = useTimeBlocks()
+  const { handleDragEnd, sensors } = useTimeBlockDND(items, setItems)
+  const hoursLeft = calcHoursLeft(items)
+
+  if (isLoading) return 'Loading'
+
+  return (
+    <div className={styles['time-block-list']}>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <div>
+          <SortableContext
+            items={items || []}
+            strategy={verticalListSortingStrategy}
+          >
+            {items?.length ? (
+              items.map((item) => (
+                <TimeBlock
+                  key={item.id}
+                  item={item}
+                  actions={[
+                    <UpdateTimeBlockButton item={item} key={item.id} />,
+                    <DeleteTimeBlockButton item={item} key={item.name} />,
+                  ]}
+                />
+              ))
+            ) : (
+              <div>You have no items</div>
+            )}
+          </SortableContext>
+        </div>
+      </DndContext>
+      <div>
+        {hoursLeft > 0
+          ? `${hoursLeft} left to sleep`
+          : 'No hours left to sleep'}
+      </div>
+    </div>
+  )
+}
+
+export default TimeBlockList
