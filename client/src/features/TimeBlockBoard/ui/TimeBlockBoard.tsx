9101f2ce97ef1152db65f9034cf2f4b9a69a37d1
Maksym Rybak <vt221_rmo@student.ztu.edu.ua>
2025-01-27 15:34:20 +0200

    add: time block board

---
+++ client/src/features/TimeBlockBoard/ui/TimeBlockBoard.tsx
@@ -0,0 +1,20 @@
+import { TimeBlockForm } from '@/features/TimeBlockForm'
+import { TimeBlockList } from '@/features/TimeBlockList'
+import { TimeBlockFormState } from '@/shared/types/time-block.types'
+import { FormProvider, useForm } from 'react-hook-form'
+import styles from './time-block-board.module.scss'
+
+function TimeBlockBoard() {
+  const formMethods = useForm<TimeBlockFormState>()
+
+  return (
+    <FormProvider {...formMethods}>
+      <div className={styles.board}>
+        <TimeBlockList />
+        <TimeBlockForm />
+      </div>
+    </FormProvider>
+  )
+}
+
+export default TimeBlockBoard
