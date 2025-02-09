2e732fab76a4ba4e7a31e115fc965eaf9af0f0e7
Maksym Rybak <maksymryb09@gmail.com>
2025-02-09 09:28:20 +0200

    add: swagger to server project

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app/app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as cookieParser from 'cookie-parser'
+import { AppModule } from './app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,6 +12,16 @@ async function bootstrap() {
     credentials: true,
     exposedHeaders: 'set-cookie',
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API Documentation')
+    .setDescription('API endpoints for the project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(process.env.PORT ?? 3000)
 }
