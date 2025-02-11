0b282d1c1410eb509feaba466c7793ab7d47ab55
Maksym Rybak <maksymrybak09@gmail.com>
2025-02-11 13:44:50 +0200

    add: users controller docs

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -11,21 +11,32 @@ import { Auth } from 'src/auth/decorators/auth.decorator'
 import { CurrentUser } from 'src/auth/decorators/user.decorator'
 import { UserDTO } from './dto/user.dto'
 import { UserService } from './user.service'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiResponse, ApiTags } from '@nestjs/swagger'
 
 @ApiTags('Users')
 @Controller('profile')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiResponse({
+    status: 200,
+    description: 'User fetched successfully.',
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   @Get()
   @Auth()
   async profile(@CurrentUser('id') id: string) {
     return this.userService.getByID(id)
   }
 
+  @ApiResponse({
+    status: 201,
+    description: 'User updated successfully.',
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   @UsePipes(new ValidationPipe())
-  @HttpCode(200)
   @Put()
   @Auth()
   async updateProfile(@CurrentUser('id') id: string, @Body() dto: UserDTO) {
