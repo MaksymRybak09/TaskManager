ee60f184284f47ef2c68a8187d5026f283550a97
Maksym Rybak <maksymrybak09@gmail.com>
2025-03-02 16:13:31 +0200

    change: pomodoro timer schema

---
+++ server/prisma/migrations/20250302131719_add_default_values_to_pomodoro_timer/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "pomodoro_timer" ALTER COLUMN "current_round" SET DEFAULT 1,
+ALTER COLUMN "is_working_time" SET DEFAULT false;
---
+++ server/prisma/migrations/20250302133149_add_rounds_to_pomodoro_timer_scheme/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "pomodoro_timer" ADD COLUMN     "rounds" INTEGER NOT NULL DEFAULT 7;
