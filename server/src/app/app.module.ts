d326d4792abc2258288d5a5617541504596682fc
Maksym Rybak <maksymrybak09@gmail.com>
2025-03-02 12:37:33 +0200

    connect: statistics module to app

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from 'src/auth/auth.module'
+import { PomodoroModule } from 'src/pomodoro/pomodoro.module'
+import { StatisticsModule } from 'src/statistics/statistics.module'
 import { TaskModule } from 'src/task/task.module'
+import { TimeBlockModule } from 'src/time-block/time-block.module'
 import { UserModule } from 'src/user/user.module'
 import { validationSchema } from './../config/config-service.config'
-import { TimeBlockModule } from 'src/time-block/time-block.module'
-import { PomodoroModule } from 'src/pomodoro/pomodoro.module'
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PomodoroModule } from 'src/pomodoro/pomodoro.module'
     TaskModule,
     TimeBlockModule,
     PomodoroModule,
+    StatisticsModule,
   ],
 })
 export class AppModule {}
