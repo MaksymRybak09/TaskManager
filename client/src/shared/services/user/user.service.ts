d0f660335bd9dec011e80a0ea0bbeb54b6146f1f
Maksym Rybak <maksymrybak09@gmail.com>
2025-03-03 12:21:03 +0200

    fix: user service types

--- client/src/shared/services/user/user.service.ts
+++ client/src/shared/services/user/user.service.ts
@@ -1,16 +1,16 @@
 import { authAxios } from '@/shared/api/interseptors'
-import { IProfileResponse, TypeUserForm } from '@/shared/types/user.types'
+import { IUser, UserForm } from '@/shared/types/user.types'
 
 class UserService {
   private baseURL = 'profile'
 
   async getProfile() {
-    const response = await authAxios.get<IProfileResponse>(this.baseURL)
+    const response = await authAxios.get<IUser>(this.baseURL)
     return response.data
   }
 
-  async update(data: TypeUserForm) {
-    const response = await authAxios.put(this.baseURL, data)
+  async update(data: UserForm) {
+    const response = await authAxios.put<UserForm>(this.baseURL, data)
     return response.data
   }
 }
