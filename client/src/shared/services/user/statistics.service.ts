f9c92b8da1f5afef0195fb97461dd44a16d944ee
Maksym Rybak <maksymrybak09@gmail.com>
2025-03-03 13:23:29 +0200

    add: statistics service

---
+++ client/src/shared/services/user/statistics.service.ts
@@ -0,0 +1,13 @@
+import { authAxios } from '@/shared/api/interseptors'
+import { IStatistics } from '@/shared/types/statistics.types'
+
+class StatisticsService {
+  private baseURL = 'statistics'
+
+  async getFullStats() {
+    const response = await authAxios.get<IStatistics>(`${this.baseURL}/full`)
+    return response.data
+  }
+}
+
+export const statisticsService = new StatisticsService()
