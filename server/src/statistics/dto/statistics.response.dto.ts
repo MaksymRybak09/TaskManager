8bd5cef1c95fd1cc8da1acad43fd350b4f0d70b6
Maksym Rybak <maksymrybak09@gmail.com>
2025-03-03 13:22:46 +0200

    change: statistics dto

--- server/src/statistics/dto/statistics.response.dto.ts
+++ server/src/statistics/dto/statistics.response.dto.ts
@@ -1,20 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNumber } from 'class-validator'
-
-export class StatisticsResponseDTO {
-  @ApiProperty({ example: 4 })
-  @IsNumber()
-  totalTasks: number
-
-  @ApiProperty({ example: 3 })
-  @IsNumber()
-  completedTasks: number
-
-  @ApiProperty({ example: 2 })
-  @IsNumber()
-  todayTasks: number
-
-  @ApiProperty({ example: 5 })
-  @IsNumber()
-  weekTasks: number
-}
+export type StatisticsResponseDTO = {
+  label: string
+  value: number
+}[]
