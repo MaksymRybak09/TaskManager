97439d5571fbbd70a1ab456c706baf754ef62e16
Maksym Rybak <maksymrybak09@gmail.com>
2025-03-09 15:46:35 +0200

    add: time block colors consts

---
+++ client/src/shared/constants/time-block-colors.ts
@@ -0,0 +1,21 @@
+import { SelectOptions } from '../types/select.types'
+
+export const TIME_BLOCK_COLORS: SelectOptions = [
+  {
+    label: 'Red',
+    value: '#FF0000',
+  },
+  {
+    label: 'Green',
+    value: '#00ff00',
+  },
+  {
+    label: 'Blue',
+    value: '#0000ff',
+  },
+]
+
+export const TIME_BLOCK_DEFAUL_COLOR = {
+  label: 'Grey',
+  value: '#f3f3f3',
+}
