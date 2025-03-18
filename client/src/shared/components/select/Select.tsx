c05f66ec8fb9ecc3a2218e6cb722878f86a1ae3b
Maksym Rybak <maksymrybak09@gmail.com>
2025-03-18 16:38:25 +0200

    change: select

--- client/src/shared/components/select/Select.tsx
+++ client/src/shared/components/select/Select.tsx
@@ -1,6 +1,7 @@
 import Badge from '@/shared/components/badge/Badge'
 import { useOutside } from '@/shared/hooks/general/use-outside'
 import type { SelectOptions } from '@/shared/types/select.types'
+import Button from '../button/Button'
 import styles from './select.module.scss'
 
 type SelectProps = {
@@ -23,16 +24,17 @@ function Select(props: SelectProps) {
       {isShow && (
         <div className={styles.child}>
           {props.data.map((item) => (
-            <Badge
+            <Button
               key={item.label}
               onClick={(event) => {
                 event.preventDefault()
                 props.onChange(item.value)
                 setIsShow(false)
               }}
+              variant="transparent"
             >
               {item.label}
-            </Badge>
+            </Button>
           ))}
         </div>
       )}
