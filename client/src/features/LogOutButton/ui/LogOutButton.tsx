52668db4af84b193729da6f5039fe681470cf14b
Maksym Rybak <maksymrybak09@gmail.com>
2025-04-01 15:33:01 +0300

    add: icon to log-out button

--- client/src/features/LogOutButton/ui/LogOutButton.tsx
+++ client/src/features/LogOutButton/ui/LogOutButton.tsx
@@ -2,10 +2,15 @@
 
 import Button from '@/shared/components/button/Button'
 import { useLogOut } from '@/shared/hooks/users/use-log-out'
+import { LogOut } from 'lucide-react'
 
 function LogOutButton() {
   const { mutate } = useLogOut()
-  return <Button onClick={() => mutate()}>Log-out</Button>
+  return (
+    <Button icon={LogOut} onClick={() => mutate()}>
+      Log-out
+    </Button>
+  )
 }
 
 export default LogOutButton
