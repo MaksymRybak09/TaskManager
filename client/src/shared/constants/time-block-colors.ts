8f02f2bd2a2f2ae9839054ddf28e8dbc8215aafb
Maksym Rybak <maksymrybak09@gmail.com>
2025-04-01 12:31:26 +0300

    change: time blocks colors

--- client/src/shared/constants/time-block-colors.ts
+++ client/src/shared/constants/time-block-colors.ts
@@ -3,19 +3,19 @@ import { SelectOptions } from '../types/select.types'
 export const TIME_BLOCK_COLORS: SelectOptions = [
   {
     label: 'Red',
-    value: '#FF0000',
+    value: '#fad4d2',
   },
   {
-    label: 'Green',
-    value: '#00ff00',
+    label: 'Pink',
+    value: '#f9d2fa',
   },
   {
-    label: 'Blue',
-    value: '#0000ff',
+    label: 'Purple',
+    value: '#ded2fa',
   },
 ]
 
 export const TIME_BLOCK_DEFAUL_COLOR = {
-  label: 'Grey',
-  value: '#f3f3f3',
+  label: 'Yellow',
+  value: '#f0fad2',
 }
