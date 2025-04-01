1f7c15e8e1ff60248870f4e351c57076674a8fe5
Maksym Rybak <maksymrybak09@gmail.com>
2025-04-01 15:32:43 +0300

    add: icon prop to button

--- client/src/shared/components/button/Button.tsx
+++ client/src/shared/components/button/Button.tsx
@@ -1,16 +1,24 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react'
 import styles from './button.module.scss'
+import { LucideIcon } from 'lucide-react'
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode
   variant?: 'transparent' | 'border' | 'filled'
+  icon?: LucideIcon
 }
 
-function Button({ children, variant = 'filled', ...props }: ButtonProps) {
+function Button({
+  children,
+  variant = 'filled',
+  icon: Icon,
+  ...props
+}: ButtonProps) {
   const classes = `${styles.button} ${styles[`${variant}-button`]}`
 
   return (
     <button className={classes} {...props}>
+      {Icon && <Icon className={styles.icon} />}
       {children}
     </button>
   )
