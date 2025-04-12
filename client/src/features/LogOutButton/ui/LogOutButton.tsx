6c0ebf05d94e7d773a295473a09361730eae97fa
Maksym Rybak <maksymrybak09@gmail.com>
2025-04-12 15:14:20 +0300

    make: log-out button responsive

--- client/src/features/LogOutButton/ui/LogOutButton.tsx
+++ client/src/features/LogOutButton/ui/LogOutButton.tsx
@@ -1,14 +1,21 @@
 'use client'
 
 import Button from '@/shared/components/button/Button'
+import { useBreakPoints } from '@/shared/hooks/general/use-break-points'
 import { useLogOut } from '@/shared/hooks/users/use-log-out'
 import { LogOut } from 'lucide-react'
 
 function LogOutButton() {
   const { mutate } = useLogOut()
+  const { isTablet } = useBreakPoints()
+
   return (
-    <Button icon={LogOut} onClick={() => mutate()}>
-      Log-out
+    <Button
+      icon={LogOut}
+      variant={isTablet ? 'border' : 'filled'}
+      onClick={() => mutate()}
+    >
+      {isTablet ? undefined : 'Log-out'}
     </Button>
   )
 }
