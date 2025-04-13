c9511295d692657e71cc3f36081759874e4198d6
Maksym Rybak <maksymrybak09@gmail.com>
2025-04-13 10:08:55 +0300

    make: menu item responsive

--- client/src/shared/components/menuItem/MenuItem.tsx
+++ client/src/shared/components/menuItem/MenuItem.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import Link from 'next/link'
-import styles from './menu-item.module.scss'
-import { usePathname } from 'next/navigation'
+import { useBreakPoints } from '@/shared/hooks/general/use-break-points'
 import { LucideIcon } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import styles from './menu-item.module.scss'
 
 type MenuItemProps = {
   label: string
@@ -15,7 +16,8 @@ function MenuItem({ icon: Icon, ...props }: MenuItemProps) {
   const pathname = usePathname()
   const isActive = pathname === props.path
 
-  const stylesString = `${styles['menu__item']} ${
+  const { isTablet } = useBreakPoints()
+  const stylesString = `${styles[isTablet ? 'tab' : 'menu__item']} ${
     isActive ? styles.active : ''
   }`
 
