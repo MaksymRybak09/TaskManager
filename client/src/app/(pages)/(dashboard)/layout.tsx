5ea3e68b9294b96d0ff7ea431add3b4146ff2aa1
Maksym Rybak <maksymrybak09@gmail.com>
2025-04-13 10:09:37 +0300

    add: footer to main layout

--- client/src/app/(pages)/(dashboard)/layout.tsx
+++ client/src/app/(pages)/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/entity/Footer'
 import styles from '@/shared/layout/HomeLayout/home-layout.module.scss'
 import { HeaderWithMenuAndLogOut } from '@/widgets/HeaderWithMenuAndLogOut'
 import { ReactNode } from 'react'
@@ -12,6 +13,9 @@ function layout(props: layoutProps) {
       <HeaderWithMenuAndLogOut />
       <div className={styles.container}>
         <main className={styles.main}>{props.children}</main>
+        <footer className={styles.footer}>
+          <Footer />
+        </footer>
       </div>
     </div>
   )
