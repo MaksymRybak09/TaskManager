46d7c7fcbe5afa1fef19ea5024c06a68d4907de7
Maksym Rybak <maksymrybak09@gmail.com>
2025-04-13 10:09:17 +0300

    make: header with features responsive

--- client/src/widgets/HeaderWithMenuAndLogOut/ui/HeaderWithMenuAndLogOut.tsx
+++ client/src/widgets/HeaderWithMenuAndLogOut/ui/HeaderWithMenuAndLogOut.tsx
@@ -1,10 +1,20 @@
+'use client'
+
 import { Header } from '@/entity/Header'
 import { Menu } from '@/entity/Menu'
 import { LogOutButton } from '@/features/LogOutButton'
+import { useBreakPoints } from '@/shared/hooks/general/use-break-points'
 
 function HeaderWithMenuAndLogOut() {
+  const { isTablet } = useBreakPoints()
+
   return (
-    <Header actions={[<Menu key="menu" />, <LogOutButton key={'log-out'} />]} />
+    <Header
+      actions={[
+        !isTablet ? <Menu key="menu" /> : undefined,
+        <LogOutButton key={'log-out'} />,
+      ]}
+    />
   )
 }
 
