5d6bb74ce66af2643126689ac616d78631ef0f6d
Maksym Rybak <maksymrybak09@gmail.com>
2025-05-06 15:12:12 +0300

    add: next auth env variables

--- client/env.d.ts
+++ client/env.d.ts
@@ -2,5 +2,9 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_URL: string
     NEXT_PUBLIC_CLIENT_HOST: string
+    GOOGLE_CLIENT_ID: string
+    GOOGLE_CLIENT_SECRET: string
+    GITHUB_CLIENT_ID: string
+    GITHUB_CLIENT_SECRET: string
   }
 }
